6200293d8e1ec34540d50cf50a0c7dde1f8b2bbf
jovanlekic12 <lekicjovan1212@gmail.com>
2024-07-30 13:35:41 +0200

    add event listeners

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,12 @@
+"use strict";
+
 import "./style.css";
+const sideBarBtn = document.querySelector(".header-btn");
+const sideBar = document.querySelector(".main-nav");
+const sideBarClose = document.querySelector(".x-icon");
+sideBarBtn.addEventListener("click", function () {
+  sideBar.classList.add("main-nav-mobile");
+});
+sideBarClose.addEventListener("click", function () {
+  sideBar.classList.remove("main-nav-mobile");
+});
